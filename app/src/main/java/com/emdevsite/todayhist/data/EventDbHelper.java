b7249f4c19f9ab91693bd33512628d0bb620eea3
Evin Dunn <evindunn14@gmail.com>
2018-04-19 08:33:55 -0700

    Reorg of EventDbHelper

--- app/src/main/java/com/emdevsite/todayhist/data/EventDbHelper.java
+++ app/src/main/java/com/emdevsite/todayhist/data/EventDbHelper.java
@@ -3,49 +3,46 @@ package com.emdevsite.todayhist.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
+
+import java.util.Locale;
 
 /**
  * Created by edunn on 3/12/18.
  * Low-level management of the event database
  */
 public class EventDbHelper extends SQLiteOpenHelper {
-
-    public static final String FILENAME = "events.db";
     private static final int VERSION = 1;   // Increment if db schema is changed
-
-    public EventDbHelper(Context context) {
-        super(context, FILENAME, null, VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        final String CREATE_TABLE_FMT =
-                "CREATE TABLE %s(" +
-                    "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "%s INTEGER NOT NULL, " +
-                    "%s TEXT UNIQUE NOT NULL, " +
-                    "%s TEXT NOT NULL" +
-                ");";
-
-        final String CREATE_TBL = String.format(
-            CREATE_TABLE_FMT,
+    private static final String FILENAME = "events.db";
+    private static final String SQL_DELETE_TBL = String.format(
+            "DROP TABLE IF EXISTS %s",
+            EventDbContract.EventTable.TABLE_NAME
+    );
+    private static final String SQL_CREATE_TBL = String.format(
+            "CREATE TABLE %s(" +
+            "%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "%s INTEGER NOT NULL, " +
+            "%s TEXT UNIQUE NOT NULL, " +
+            "%s TEXT NOT NULL" +
+            ");",
             EventDbContract.EventTable.TABLE_NAME,
             EventDbContract.EventTable._ID,
             EventDbContract.EventTable.COLUMN_DATE,
             EventDbContract.EventTable.COLUMN_YEAR,
             EventDbContract.EventTable.COLUMN_TEXT
-        );
+    );
 
-        db.execSQL(CREATE_TBL);
+    EventDbHelper(Context context) {
+        super(context, FILENAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TBL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        final String DELETE_TABLE_FMT =
-                "DROP TABLE IF EXISTS %s";
-
-        db.execSQL(String.format(DELETE_TABLE_FMT, EventDbContract.EventTable.TABLE_NAME));
+        db.execSQL(SQL_DELETE_TBL);
         onCreate(db);
     }
 }
